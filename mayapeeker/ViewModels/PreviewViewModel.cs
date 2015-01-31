b5fe494d81a456a2179a2712bb72b9efbe2e97af
hal1932 <yu.arai.19@gmail.com>
2015-01-31 18:09:50 +0900

    ゴミが混ざったので削除

--- mayapeeker/ViewModels/PreviewViewModel.cs
+++ mayapeeker/ViewModels/PreviewViewModel.cs
@@ -6,23 +6,10 @@ namespace mayapeeker.ViewModels
 {
     class PreviewViewModel : ViewModelBase
     {
-        public PreviewViewModel()
-        {
-            Messenger.AddMessageFilter("CurrentDirectoryChanged");
-            Messenger.MessageReceived += (sender, e) =>
-            {
-                _currentDirectory = e.Content as DirectoryInfo;
-            };
-        }
-
-
         public override void Initialize()
         {
             base.Initialize();
         }
-
-
-        private DirectoryInfo _currentDirectory;
 
     }
 }
